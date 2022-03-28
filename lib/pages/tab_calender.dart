dfd65d08fa30e76f2d2edf8434b62478f0695b70
dmjang <dogzz9445@gmail.com>
2022-03-28 13:07:57 +0900

    Add table_calendar

--- lib/pages/tab_calender.dart
+++ lib/pages/tab_calender.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 /// A page that shows a summary of bills.
 class CalenderView extends StatefulWidget {
@@ -10,13 +11,26 @@ class CalenderView extends StatefulWidget {
 
 class _CalenderViewState extends State<CalenderView>
     with SingleTickerProviderStateMixin {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
   @override
   Widget build(BuildContext context) {
-    return CalendarDatePicker(
-      firstDate: DateTime(2017),
-      initialDate: DateTime.now(),
-      lastDate: DateTime(2030),
-      onDateChanged: (DateTime value) {},
-    );
+    // return CalendarDatePicker(
+    //   firstDate: DateTime(2017),
+    //   initialDate: DateTime.now(),
+    //   lastDate: DateTime(2030),
+    //   onDateChanged: (DateTime value) {},
+    // );
+    return TableCalendar(
+        firstDay: DateTime.utc(2017),
+        lastDay: DateTime.utc(2030),
+        focusedDay: _focusedDay,
+        onDaySelected: (selectedDay, focusedDay) {
+          setState(() {
+            _selectedDay = selectedDay;
+            _focusedDay = focusedDay; // update `_focusedDay` here as well
+          });
+        });
   }
 }
